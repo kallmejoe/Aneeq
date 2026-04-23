7347e92530631cb57e1f75b3b032e6889a617d9c
Loay Khaled <lokamohammed43@gmail.com>
2026-04-23 16:23:59 +0200

    feature: db schema

---
+++ server/utils/init.ts
@@ -0,0 +1,90 @@
+import { db } from "./db";
+
+db.exec(`
+/*
+    Notes:
+    dates text refrences ISO dates
+    i am feeling sick and sleepy so I'll delat the 
+    writing of documentation for each table,
+     I will do it in a later commit
+*/
+
+-- users table
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    role TEXT NOT NULL CHECK(role IN ('professor', 'student', 'admin'))
+);
+
+-- proffesor information
+CREATE TABLE professors_info (
+    user_id INTEGER PRIMARY KEY,
+    department TEXT,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- student information 
+CREATE TABLE students_info (
+    user_id INTEGER PRIMARY KEY,
+    major TEXT,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- courses
+CREATE TABLE courses (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    professor_id INTEGER NOT NULL,
+    FOREIGN KEY (professor_id) REFERENCES professors_info(user_id) ON DELETE RESTRICT
+);
+
+-- course_time_slots
+CREATE TABLE course_time_slots (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    course_id INTEGER NOT NULL,
+    day_of_week TEXT NOT NULL,
+    start_time TEXT NOT NULL, 
+    end_time TEXT NOT NULL,   
+    FOREIGN KEY (course_id) REFERENCES courses(id) ON DELETE CASCADE
+);
+
+CREATE TABLE professor_av_time_slots (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    professor_id INTEGER NOT NULL,
+    day_of_week TEXT NOT NULL,
+    start_time TEXT NOT NULL, 
+    end_time TEXT NOT NULL,  
+    FOREIGN KEY (professor_id) REFERENCES professor_info(user_id) ON DELETE CASCADE
+);
+
+-- courses students enrollments  
+-- not sure if on delete cascade would be correct here
+CREATE TABLE course_enrollments (
+    student_id INTEGER NOT NULL,
+    course_id INTEGER NOT NULL,
+    PRIMARY KEY (student_id, course_id),
+    FOREIGN KEY (student_id) REFERENCES students_info(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (course_id) REFERENCES courses(id) ON DELETE CASCADE
+);
+
+CREATE TABLE assignments (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    course_id INTEGER NOT NULL,
+    title TEXT NOT NULL,
+    description TEXT,
+    due_date TEXT, 
+    FOREIGN KEY (course_id) REFERENCES courses(id) ON DELETE CASCADE
+);
+
+CREATE TABLE assignment_grades (
+    student_id INTEGER NOT NULL,
+    assignment_id INTEGER NOT NULL,
+    grade REAL,
+    PRIMARY KEY (student_id, assignment_id),
+    FOREIGN KEY (student_id) REFERENCES students_info(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (assignment_id) REFERENCES assignments(id) ON DELETE CASCADE
+);
+
+`);
